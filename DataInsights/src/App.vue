d8e47c5be6e82b655812412e585f2e7e28f5412f
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-03-21 16:50:18 +0200

    Create App.vue

---
+++ DataInsights/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MenuDisplay from './components/MenuDisplay.vue';
+import pieChart from './components/MainInfo.vue'
+import PieChart from './components/PieChart.vue';
+</script>
+
+<template>
+  <header>
+  
+    <MenuDisplay />
+  </header>
+
+  <main>
+    <pieChart/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
