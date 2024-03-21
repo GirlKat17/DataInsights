1c74a4b8319582917e413e5c9855298cfc52f929
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-03-21 16:49:47 +0200

    Create PieChart.vue

---
+++ DataInsights/src/components/PieChart.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <canvas ref="pieChartCanvas"></canvas>
+    </div>
+  </template>
+  
+  <script>
+  import { Pie, mixins } from 'vue-chartjs';
+  const { reactiveProp } = mixins;
+  
+  export default {
+    extends: Pie,
+    mixins: [reactiveProp],
+    props: ['chartData', 'options'],
+    mounted() {
+      this.renderPieChart();
+    },
+    watch: {
+      chartData() {
+        this.renderPieChart();
+      },
+    },
+    methods: {
+      renderPieChart() {
+        this.renderChart(this.chartData, this.options);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
