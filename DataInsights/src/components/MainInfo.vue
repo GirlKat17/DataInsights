047189430c9f06f8c99250b0560c72dccb52c202
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-03-21 16:49:55 +0200

    Create MainInfo.vue

---
+++ DataInsights/src/components/MainInfo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <pie-chart :chartData="pieChartData" :options="pieChartOptions"></pie-chart>
+    </div>
+  </template>
+  
+  <script>
+  import PieChart from './components/PieChart.vue';
+  import pieChartData from '/pieChart.json'; 
+  
+  export default {
+    components: {
+      PieChart,
+    },
+    data() {
+      return {
+        pieChartData: pieChartData,
+        pieChartOptions: {} 
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
